8f3bf4d4ba31e2408c1cbaddebf35d6db0608ced
Caleb Norton <n0603919@outlook.com>
2024-11-20 18:44:02 -0600

    Add workshop 1 file

---
+++ workshop1/workshop1.ino
@@ -0,0 +1,48 @@
+/*
+Arudino Code created for the ARM x IEEE Workshop
+Author: Akshat Nerella
+Purpose: The code forwards all accelerometer outputs in a tsv format
+for the data forwarder to forward this data to the Edge Impulse CLI
+*/
+
+
+
+
+#include <Wire.h>
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+
+
+void setup(void) {
+  Serial.begin(15200);
+ 
+//  Serial.println("Adafruit MMA8451 test!");
+ 
+
+
+  if (! mma.begin()) {
+//    Serial.println("Couldnt start");
+    while (1);
+  }
+//  Serial.println("MMA8451 found!");
+ 
+  mma.setRange(MMA8451_RANGE_2_G);
+ 
+//  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
+//  Serial.println("G");
+ 
+}
+
+
+void loop() {
+  // Read the 'raw' data in 14-bit counts
+  mma.read();
+  Serial.print(mma.x);
+  Serial.print("\t"); Serial.print(mma.y);
+  Serial.print("\t"); Serial.print(mma.z);
+  Serial.println();
+  delay(100);
+}
